03ed71d42ca962ff5c8ae49bacd6a0c7a42c7099
SasaAndjelkovic <sandjelkovickg@gmail.com>
2022-10-17 12:46:47 +0200

    focus blur keypress hide validacija

--- scripts/scriptIndex.js
+++ scripts/scriptIndex.js
@@ -1,10 +1,33 @@
 $(document).ready(function(){
-    $("#fname").focus(function(){
-        $(this).css("background-color", "#cccccc");
+    $("#fname").on("focus blur", function (e) {
+        if (e.type == "focus") {
+            $(this).css("background-color", "#ed7b7b");
+        } else {
+            $(this).css("background-color", "#ffffff");
+        }
+    });
+    $("#passw").on("focus blur", function (e) {
+        if (e.type == "focus") {
+            $(this).css("background-color", "#ed7b7b");
+        } else {
+            $(this).css("background-color", "#ffffff");
+        }
+    });
+    $("input").keypress(function(){
+        if ($(this).val().length > 3) {
+            $(this).css("background-color", "rgba(32, 145, 7, 0.5)");
+        };
     });
 });
 
-$("submit").click(function(e){
+$("input[value='posalji']").click(function (e) {
     e.preventDefault();
     $(".wrap").hide();
+    let $text = "";
+    if ($("#fname").val() < 5 || $("#passw").val() < 5) {
+        $text = "Korisnicko ime ili lozinka su kraci od 5 karaktera!"
+    } else {
+        $text = $("#fname").val();
+    }
+    $(".pozdrav").text("Drugi P" + $text);
 });
