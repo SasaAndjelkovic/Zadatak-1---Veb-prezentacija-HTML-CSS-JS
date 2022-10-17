1af61c36e8144da64c4232f85fe68801cc592f5f
SasaAndjelkovic <sandjelkovickg@gmail.com>
2022-10-17 16:44:38 +0200

    dosta muka sa #active, .active; resenje hide/show

---
+++ scripts/scriptAbout.js
@@ -0,0 +1,38 @@
+$(".m1avatar").hide();
+$(".m2avatar").hide();
+$(".f1avatar").hide();
+$(".f2avatar").hide();
+
+$( "input" ).on( "click", function(e) {
+  e.preventDefault(); 
+  $(".izaberi").hide();
+
+  switch ($( "input[type='radio']:checked" ).val()) {
+    case "m1avatar":
+      $(".m1avatar").show();
+      break;
+    case "m2avatar":
+     
+      $(".m2avatar").show();
+      break;
+    case "f1avatar":
+      $(".f1avatar").show();
+      break;
+    case "f2avatar":
+      $(".f2avatar").show();
+      break
+    default:
+      console.log("Greska")
+  }
+});
+
+ // if ($id == "m1avatar") {
+    //   console.log("super");
+    //   $(".m1avatar").hide();
+    // }
+
+  // let $id = $( "input[type='radio']:checked" ).val();
+// $(".pot").click(function(e) {
+//   e.preventDefault();
+  
+ // $(".m2avatar").addClass(".active");
